fb4ff927cdf83cf5ba8bb9cc0c5bf628928b8008
zsj <lekyzsj@gmail.com>
2019-01-16 19:18:59 +0800

    Set configuration from ENV variables for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,8 @@ require "minitest/autorun"
 require "securerandom"
 
 QiniuStorage.configure do |config|
-  config.use_https = false
-  config.use_cdn = false
-  config.debug_mode = false
+  config.use_https = ENV.fetch("QINIU_USE_HTTPS", false)
+  config.use_cdn = ENV.fetch("QINIU_USE_CDN", false)
+  config.debug_mode = ENV.fetch("QINIU_DEBUG_MODE", false)
   config.upload_max_threads = 5
 end
